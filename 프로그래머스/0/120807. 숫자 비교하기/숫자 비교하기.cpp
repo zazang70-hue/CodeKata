72125e571126db0e7d7c94fd5a6b782bd4747c31
zazang70-hue <zazang70@gmail.com>
2025-12-29 09:17:36 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -7,7 +7,8 @@ int solution(int num1, int num2)
 {
     int answer = 0;
     
-
+    if (num1 >= 0 && num1 <= 10000 && num2 >= 0 && num2 <= 10000)
+    {
         if (num1 != num2)
         {
             answer = -1;
@@ -16,7 +17,7 @@ int solution(int num1, int num2)
         {
             answer = 1;
         }
-
+    }
     
     return answer;
 }
