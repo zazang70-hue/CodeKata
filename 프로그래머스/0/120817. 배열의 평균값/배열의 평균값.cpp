6f6127c78881b53a70f13fafd9137510da64b941
zazang70-hue <zazang70@gmail.com>
2026-01-06 09:38:08 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers)
+{
+	double answer = 0;
+	double sum = 0;
+
+	for (int i = 0; i < numbers.size(); ++i)
+	{
+		sum += numbers[i];
+	}
+
+	return answer = sum / numbers.size();
+}
