ea848a99fd8564f2e16ea6ce26eb4de50b6d0f69
zazang70-hue <zazang70@gmail.com>
2026-01-27 09:57:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int>absolutes, vector<bool>signs)
+{
+    int sum = 0;
+
+    for (int i = 0; i < signs.size(); ++i)
+    {
+        if (signs[i] == false)
+        {
+            absolutes[i] = -absolutes[i];
+            sum += absolutes[i];
+        }
+        else
+        {
+            sum += absolutes[i];
+        }
+    }
+
+    return sum;
+}
