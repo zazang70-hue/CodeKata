15e14f6ac7c9748c15eab0e34a2a3377796ffe52
zazang70-hue <zazang70@gmail.com>
2026-02-03 09:12:04 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.10 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -8,8 +8,14 @@ string solution(int n)
 
     for (int i = 1; i <= n; ++i)
     {
-        
-        answer += (i % 2 != 0) ? "수" : "박" ;
+        if (i % 2 != 0)
+        {
+            answer += "수";
+        }
+        else
+        {
+            answer += "박";
+        }
     }
 
     return answer;
