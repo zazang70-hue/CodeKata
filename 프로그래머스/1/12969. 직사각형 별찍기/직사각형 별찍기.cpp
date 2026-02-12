7360f53489f463e138242051616f3afcf75482ab
jeahuyn Lee <zazang70@gmail.com>
2026-02-12 09:36:54 +0900

    [level 1] Title: 직사각형 별찍기, Time: 4.38 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    int m;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
